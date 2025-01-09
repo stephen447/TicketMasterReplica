48cf0f87b77f4a8d9bebae27e5760b7112a4aedc
stephen447 <byrnes67@tcd.ie>
2025-01-09 18:59:51 +0000

    tailwind set up

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -2,7 +2,7 @@ import { JSX } from "react";
 export default function Homepage(): JSX.Element {
   return (
     <div>
-      <p>TicketMaster</p>
+      <p className="bg-blue-500">TicketMaster</p>
       <a href="https://www.linkedin.com/in/stephen-byrne-b4729321b/">
         LinkedIn
       </a>
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Include all JS/TSX files in the src folder
+  ],
+  theme: {
+    extend: {}, // Extend theme settings here if needed
+  },
+  plugins: [],
+};
